60a1db48ca48a3e1b905633f93f7cc037d3b0c3b
mmaitre314 <mmaitre314@users.noreply.github.com>
2016-01-07 22:16:42 -0800

    Initial commit

---
+++ Demo/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ((Storyboard)FindResource("WaitStoryboard")).Begin();
+        }
+    }
+}
